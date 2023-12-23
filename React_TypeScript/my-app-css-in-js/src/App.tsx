92212957868e5db9053ddab6b2f7df839d6eff5a
HYESOO KIM <khwater1017@naver.com>
2023-12-23 21:30:02 +0900

    react CSS 적용

---
+++ React_TypeScript/my-app-css-in-js/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import styled from '@emotion/styled';
+
+const Contaniner = styled.div`text-align: center;`;
+const Header = styled.header `
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  `;
+function App() {
+  return (
+    //<div className="App">
+    <Contaniner>
+      {/* <header className="App-header"> */}
+      <Header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      {/* </header> */}
+      </Header>
+      </Contaniner>
+    //</div>
+  );
+}
+
+export default App;
